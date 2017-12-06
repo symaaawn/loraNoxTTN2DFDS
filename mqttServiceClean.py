3d1f4df06b19244ef01a43f07f4892045a1f530a
symaaawn <simon.neugebauer@gmail.com>
2017-12-06 14:54:31 +0100

    Add files via upload
    cleaned up and check for port before converting and publishing

--- mqttServiceClean.py
+++ mqttServiceClean.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
 #	DatenFuerDieStadt MQTT Service
-#	Receives MQTT 
-#
-#
+#	Subscribes to The Things Network MQTT Broker for all uplink messages, converts them into a
+#       Format the SensorCloud of the TH Köln can understand and publishes them to the SensorCloud
+#       Broker
+
 import time
 import paho.mqtt.client as mqtt
 import json
@@ -13,7 +14,6 @@ gwID = "bcff9a23-47f6-489f-a1ba-b6c26723cc70"
 
 def get_senID(devEUI):
 	return { 
-		"000DCBC189D013AD": "e6bfb32e-9e0f-4b20-8664-f9ce5229204b", 
 		"000C97C70C6FEEC4": "e6bfb32e-9e0f-4b20-8664-f9ce5229204b"
 	}.get(devEUI)
 
@@ -33,35 +33,36 @@ def on_message(client, userdata, msg):
 	print("\nMessage received")
 	print(json.dumps(parsedPayload, sort_keys = True, indent = 4))
 	
-	dfdsMessage = {}
-	dfdsMessage["typ"] = "1"
-	dfdsMessage["gw"] = gwID
-	dfdsMessage["bn"] = senID
-	dfdsMessage["bt"] = int(time.time()) * 1000	
-	
-	#erster Messwert: nox
-	noxData = {}
-	noxData["n"] = str(uuid.uuid4()) + ":R1"
-	noxData["t"] = 0
-	try:
-		noxData["sv"] = parsedPayload["payload_fields"]["rNox"]
-	except:
-		noxData["sv"] = 0
-	
-	#zweiter Messwert: co
-	coData = {}
-	coData["n"] = str(uuid.uuid4()) + ":R2"
-	coData["t"] = 0
-	try:
-		coData["sv"] = parsedPayload["payload_fields"]["coData"]
-	except:
-		coData["sv"] = 0
-		
-	dfdsMessage["e"] = [noxData, coData]
-	
-	dfdsClient.publish("mw/sc/" + senID, json.dumps(dfdsMessage))
-	print("Message published")
-	print(json.dumps(dfdsMessage, sort_keys = True, indent = 4))
+	if (parsedPayload["port"] == 3):
+            dfdsMessage = {}
+            dfdsMessage["typ"] = "1"
+            dfdsMessage["gw"] = gwID
+            dfdsMessage["bn"] = senID
+            dfdsMessage["bt"] = int(time.time()) * 1000	
+            
+            #erster Messwert: nox
+            noxData = {}
+            noxData["n"] = str(uuid.uuid4()) + ":R1"
+            noxData["t"] = -3000
+            try:
+                    noxData["sv"] = parsedPayload["payload_fields"]["rNox"]
+            except:
+                    noxData["sv"] = 0
+            
+            #zweiter Messwert: co
+            coData = {}
+            coData["n"] = str(uuid.uuid4()) + ":R2"
+            coData["t"] = -3000
+            try:
+                    coData["sv"] = parsedPayload["payload_fields"]["coData"]
+            except:
+                    coData["sv"] = 0
+                    
+            dfdsMessage["e"] = [noxData, coData]
+            
+            dfdsClient.publish("mw/sc/" + senID, json.dumps(dfdsMessage))
+            print("Message published")
+            print(json.dumps(dfdsMessage, sort_keys = True, indent = 4))
 	
 ttnClient = mqtt.Client()
 ttnClient.on_connect = on_ttn_connect
