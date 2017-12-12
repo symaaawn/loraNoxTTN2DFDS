9ee0839de44bd1535a34253a1613e97045e13abc
symaaawn <simon.neugebauer@gmail.com>
2017-12-12 14:32:05 +0100

    Add files via upload
    added more sensor values. Is now ready for OpenAir Node v1.1

--- mqttServiceClean.py
+++ mqttServiceClean.py
@@ -14,7 +14,8 @@ gwID = "bcff9a23-47f6-489f-a1ba-b6c26723cc70"
 
 def get_senID(devEUI):
 	return { 
-		"000C97C70C6FEEC4": "e6bfb32e-9e0f-4b20-8664-f9ce5229204b"
+		"00AA76B1E9DEA616": "e6bfb32e-9e0f-4b20-8664-f9ce5229204b",
+                "0014F1CAE3D257C1": "f61ae9b4-0d74-4d0b-8a67-602290d61425"
 	}.get(devEUI)
 
 def on_ttn_connect(client, userdata, flags, rc):
@@ -33,32 +34,64 @@ def on_message(client, userdata, msg):
 	print("\nMessage received")
 	print(json.dumps(parsedPayload, sort_keys = True, indent = 4))
 	
-	if (parsedPayload["port"] == 3):
+	if (parsedPayload["port"] == 1):
             dfdsMessage = {}
             dfdsMessage["typ"] = "1"
             dfdsMessage["gw"] = gwID
             dfdsMessage["bn"] = senID
             dfdsMessage["bt"] = int(time.time()) * 1000	
+            dfdsMessage["e"] = []
             
             #erster Messwert: nox
-            noxData = {}
-            noxData["n"] = str(uuid.uuid4()) + ":R1"
-            noxData["t"] = -3000
-            try:
-                    noxData["sv"] = parsedPayload["payload_fields"]["rNox"]
-            except:
-                    noxData["sv"] = 0
+            if "rNox" in parsedPayload["payload_fields"]:
+                noxData = {}
+                noxData["n"] = str(uuid.uuid4()) + ":R1"
+                noxData["t"] = -3000
+                noxData["sv"] = parsedPayload["payload_fields"]["rNox"]
+                dfdsMessage["e"].append(dict(noxData))
             
             #zweiter Messwert: co
-            coData = {}
-            coData["n"] = str(uuid.uuid4()) + ":R2"
-            coData["t"] = -3000
-            try:
-                    coData["sv"] = parsedPayload["payload_fields"]["coData"]
-            except:
-                    coData["sv"] = 0
-                    
-            dfdsMessage["e"] = [noxData, coData]
+            if "rCo" in parsedPayload["payload_fields"]:
+                coData = {}
+                coData["n"] = str(uuid.uuid4()) + ":R2"
+                coData["t"] = -3000
+                coData["sv"] = parsedPayload["payload_fields"]["rCo"]
+                dfdsMessage["e"].append(dict(coData))
+                              
+            if "temp" in parsedPayload["payload_fields"]:
+                tempData = {}
+                tempData["n"] = str(uuid.uuid4()) + ":temp"
+                tempData["t"] = -3000
+                tempData["sv"] = parsedPayload["payload_fields"]["temp"]
+                dfdsMessage["e"].append(dict(tempData))
+                
+            if "rssi" in parsedPayload["payload_fields"]:
+                rssiData = {}
+                rssiData["n"] = str(uuid.uuid4()) + ":rssi"
+                rssiData["t"] = -3000
+                rssiData["sv"] = parsedPayload["payload_fields"]["rssi"]
+                dfdsMessage["e"].append(dict(rssiData))
+                
+            if "hum" in parsedPayload["payload_fields"]:
+                humData = {}
+                humData["n"] = str(uuid.uuid4()) + ":hum"
+                humData["t"] = -3000
+                humData["sv"] = parsedPayload["payload_fields"]["hum"]
+                dfdsMessage["e"].append(dict(humData))
+                
+            if "pm10" in parsedPayload["payload_fields"]:
+                pm10Data = {}
+                pm10Data["n"] = str(uuid.uuid4()) + ":pm10"
+                pm10Data["t"] = -3000
+                pm10Data["sv"] = parsedPayload["payload_fields"]["pm10"]
+                dfdsMessage["e"].append(dict(pm10Data))
+                
+            if "pm25" in parsedPayload["payload_fields"]:
+                pm25Data = {}
+                pm25Data["n"] = str(uuid.uuid4()) + ":pm25"
+                pm25Data["t"] = -3000
+                pm25Data["sv"] = parsedPayload["payload_fields"]["pm25"]
+                dfdsMessage["e"].append(dict(pm25Data))
             
             dfdsClient.publish("mw/sc/" + senID, json.dumps(dfdsMessage))
             print("Message published")
@@ -74,6 +107,9 @@ dfdsClient = mqtt.Client()
 dfdsClient.on_connect = on_dfds_connect
 dfdsClient.username_pw_set("user", "pw")
 dfdsClient.connect("babeauf.nt.fh-koeln.de", 2883, 60)
+
+#dfdsClient.loop_start()
+#ttnClient.loop_start()
 
 while True:
     dfdsClient.loop()
